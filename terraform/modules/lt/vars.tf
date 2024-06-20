dc9e8dd124eb60eadc4050403aa3e9b8b5775311
saudk00 <saud.kang12@gmail.com>
2024-06-20 05:01:10 +0000

    updated2

--- terraform/modules/lt/vars.tf
+++ terraform/modules/lt/vars.tf
@@ -1,10 +1,10 @@
 variable "image_id" {
   type = string
   description = "image id"
+  default = "ami-08a0d1e16fc3f61ea"
 }
 
 variable "instance_type" {
   type = string
   description = "instance type of lt"
 }
-
